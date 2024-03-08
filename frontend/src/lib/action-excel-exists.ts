9bc46b4e923ca5eba0c603e7d016f1627884762c
Jonas Haven <j.haven@osnanet.de>
2024-03-08 01:53:26 +0100

    fix(structure): remove wrong action declaration

--- frontend/src/lib/action-excel-exists.ts
+++ frontend/src/lib/action-excel-exists.ts
@@ -1,9 +1,9 @@
-"use server";
-
 import { ExcelFileName } from "@/types";
 import { existsSync } from "fs";
 import { excelFileLocation } from "./excel-file-location";
 
 export const excelExists = (type: ExcelFileName): boolean => {
-  return existsSync(excelFileLocation(type));
+  const exists = existsSync(excelFileLocation(type));
+  console.log("🚀 ~ excelExists ~ exists:", exists);
+  return exists;
 };
